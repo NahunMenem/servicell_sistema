32d3755b57f28293524acc3f4161f0df1813f1c9
NahunMenem <carlosnahunmenem@gmail.com>
2025-02-28 12:44:19 -0300

    base

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session,flash
+from flask import Flask, render_template, request, redirect, url_for, session,flash,jsonify
 import sqlite3
 from datetime import datetime, timedelta
 
